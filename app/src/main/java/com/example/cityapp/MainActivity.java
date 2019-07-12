bbb0ddf4dea56dba77bd9eea50f5528267993a13
abdul <abdul.tanveer@gmail.com>
2019-07-12 08:43:12 +0530

    recyclerview and adapter

--- app/src/main/java/com/example/cityapp/MainActivity.java
+++ app/src/main/java/com/example/cityapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -10,6 +12,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.cityapp.adapters.RvAdapter;
 import com.example.cityapp.data.City;
 
 import org.json.JSONArray;
@@ -22,11 +25,16 @@ public class MainActivity extends AppCompatActivity {
     private String URLstring = "https://demonuts.com/Demonuts/JsonTest/Tennis/json_parsing.php";
     public static String TAG = MainActivity.class.getSimpleName();
     LinkedList<City> cityLinkedList;
+    RvAdapter adapter;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cityLinkedList = new LinkedList<>();
+         recyclerView = findViewById(R.id.recycler);
+
+
         fetchingJSON();
     }
 
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         Log.d(TAG, ">>" + response);
                         parseJson(response);
+                        setupRecycler();
 
                     }
                 }, new Response.ErrorListener() {
@@ -70,6 +79,14 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setupRecycler(){
+
+        adapter = new RvAdapter(this,cityLinkedList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
     }
 
 }
